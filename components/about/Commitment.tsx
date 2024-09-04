185925af7a5f7ab77f01ea4e4af242f83bff081c
Radu Baroncea <baroncea.radu@gmail.com>
2024-09-04 17:16:23 +0300

    fix screen width for commit

--- components/about/Commitment.tsx
+++ components/about/Commitment.tsx
@@ -2,14 +2,35 @@
 
 import { useState, useEffect } from "react";
 
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
+
 function Commitment() {
-  const [screenWidth, setScreenWidth] = useState(0);
-
-  const updateWindowSize = () => {
-    setScreenWidth(window.innerWidth);
-  };
-
-  useEffect(() => (window.onresize = updateWindowSize), []);
+  const { width: screenWidth } = useWindowSize();
 
   const returnBackgroundImg = () => {
     if (screenWidth >= 1024)
